0b9b2ceb5f851d16efb577da32591ef741b6b8e3
spencrr <23708360+spencrr@users.noreply.github.com>
2019-05-20 17:12:48 -0500

    add todos on frontend

--- client/src/router.js
+++ client/src/router.js
@@ -1,29 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import Shop from './views/Shop.vue'
-import Auth from './views/Auth.vue'
+import Todos from "./views/Todos.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [{
-    path: '/',
-    name: 'home',
-    component: Home
-  }, {
-    path: '/about',
-    name: 'about',
-    component: About
-  }, {
-    path: '/shop',
-    name: 'shop',
-    component: Shop
-  }, {
-    path: '/auth',
-    name: 'auth',
-    component: Auth
-  }],
-})
+  routes: [
+    {
+      path: "/",
+      name: "todos",
+      component: Todos
+    }
+  ]
+});
