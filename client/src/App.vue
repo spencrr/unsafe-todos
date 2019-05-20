91750bec540ed2fa5a95a5f071a8d2e503f4ada5
Spencer Schoenberg <spencers0730@gmail.com>
2019-05-20 17:12:48 -0500

    add todos on frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/shop">Shop</router-link>
-      <router-link to="/auth">Auth</router-link>
+      <router-link to="/">Todos</router-link>
     </div>
     <router-view/>
   </div>
---
+++ client/src/views/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="todos">
+    <ul>
+      <li :key="todo.id" v-for="todo in todos">{{todo.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "todos",
+  data() {
+    return {
+      todos: []
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3000/todos", {
+        headers: { "Access-Control-Allow-Origin": "*" }
+      })
+      .then(res => {
+        this.todos = res.data;
+      });
+  }
+};
+</script>
