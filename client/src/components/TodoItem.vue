409114f3cf78a41a90f3e8e7e6c2464c15978c83
spencrr <23708360+spencrr@users.noreply.github.com>
2019-05-20 17:34:22 -0500

    todoItem implemented
    still need work on css and modifying it tho

---
+++ client/src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="todo-item">
+    <h2 class="todo-item-title">{{todo.name}}</h2>
+    <div class="todo-item-info">
+      <div class="todo-item-desc">{{todo.description}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todoItem",
+  props: ["todo"]
+};
+</script>
+ 
+<style>
+.todo-item {
+  width: 25vw;
+  margin: 5px;
+  border-radius: 10px;
+  background-image: linear-gradient(to bottom right, , #ffc4007e);
+  padding: 20px;
+}
+
+/* .todo-item-info {
+  display: flex;
+  justify-content: space-between;
+} */
+
+.todo-item-desc {
+  margin-right: 20px;
+}
+</style>
--- client/src/views/Todos.vue
+++ client/src/views/Todos.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="todos">
-    <ul>
-      <li :key="todo.id" v-for="todo in todos">{{todo.name}}</li>
-    </ul>
+    <TodoItem :todo="todo" :key="todo.id" v-for="todo in todos"/>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import TodoItem from "@/components/TodoItem";
 
 export default {
   name: "todos",
@@ -24,6 +23,18 @@ export default {
       .then(res => {
         this.todos = res.data;
       });
+  },
+  components: {
+    TodoItem
   }
 };
 </script>
+
+<style scoped>
+.todos {
+  display: flex;
+  width: 75vw;
+  margin: 0 auto;
+}
+</style>
+
