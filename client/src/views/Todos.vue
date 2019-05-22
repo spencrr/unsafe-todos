25795c91cefb9a4bf8a9be372f5773d9ac47c3a4
Spencer Schoenberg <spencers0730@gmail.com>
2019-05-22 16:11:00 -0500

    fully integrated (just need express static files)

--- client/src/views/Todos.vue
+++ client/src/views/Todos.vue
@@ -1,40 +1,78 @@
 <template>
   <div class="todos">
-    <TodoItem :todo="todo" :key="todo.id" v-for="todo in todos"/>
+    <Add @add="fillTodos"/>
+    <button @click="fillTodos">Add</button>
+    <br>
+    <button @click="delTodos">Delete</button>
+    <div class="todo-items">
+      <TodoItem
+        :todo="todo"
+        :key="todo.id"
+        v-for="todo in todos"
+        @delete="fillTodos"
+        @editing="shouldPoll=false"
+        @doneEditing="shouldPoll=true"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import TodoItem from "@/components/TodoItem";
+import Add from "@/components/Add";
 
 export default {
   name: "todos",
   data() {
     return {
-      todos: []
+      todos: [],
+      polling: null,
+      shouldPoll: true
     };
   },
   created() {
-    axios
-      .get("http://localhost:3000/todos", {
-        headers: { "Access-Control-Allow-Origin": "*" }
-      })
-      .then(res => {
-        this.todos = res.data;
+    this.fillTodos();
+    this.polling = setInterval(() => {
+      if (this.shouldPoll) {
+        this.fillTodos();
+      }
+    }, 10000);
+  },
+  destroyed() {
+    clearInterval(this.polling);
+  },
+  methods: {
+    fillTodos() {
+      axios.get("http://localhost:3000/todos").then(res => {
+        this.todos = res.data.map(e => {
+          e.done = e.done === "true";
+          return e;
+        });
       });
+    },
+    delTodos() {
+      let requests = this.todos.map(todo =>
+        axios.delete(`http://localhost:3000/todos/${todo.id}`)
+      );
+      axios.all(requests).then(() => (this.todos = []));
+    }
   },
   components: {
-    TodoItem
+    TodoItem,
+    Add
   }
 };
 </script>
 
 <style scoped>
-.todos {
+.todo-items {
   display: flex;
   width: 75vw;
   margin: 0 auto;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  transition: cubic-bezier(0.075, 0.82, 0.165, 1);
 }
 </style>
 
